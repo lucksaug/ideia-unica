3ddeb958a4f7c24e985b6bfe5aa6596bbb4e29ec
lucksaug <lucaug2000@outlook.com>
2024-04-15 19:01:01 -0300

    Adicionando revalidade

--- pages/tempo.js
+++ pages/tempo.js
@@ -11,15 +11,17 @@ function Tempo(props){
     )
 }
 
-export function getStaticProps(){
+export async function getStaticProps(){
     console.log('> Passando pelo getStaticProps();')
+    
     const staticDate = new Date();
     const staticDateString = staticDate.toGMTString();
 
     return{
         props:{
             staticDateString
-        }
+        },
+        revalidate: 1
     }
 }
 
