23f64139f48c8661541fd47a9f5147c98ecc7636
lucksaug <lucaug2000@outlook.com>
2024-04-19 13:08:16 -0300

    Variável de ambiente

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,11 +1,17 @@
-function tempo(request, response){
-    const dynamicDate = new Date()
+async function tempo(request, response){
+    const apiSecret = process.env.CONVERTKIT_API_SECRET;
+    const dynamicDate = new Date();
 
-    response.json(
-        {
-            date: dynamicDate.toGMTString()
-        }
-    )
+    const subscribersResponse = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiSecret}`);
+    const subscribersResponseJson = await subscribersResponse.json();
+    const dia = subscribersResponseJson.date;
+
+    response.json({
+        date: dynamicDate.toGMTString(),
+        data: dia
+    });
 }
 
 export default tempo;
+
+//https://api.nasa.gov/planetary/apod?api_key=7dMPn2A8LlX8dmIchnpv5hyDLnBDvd4fwmmVIThg
